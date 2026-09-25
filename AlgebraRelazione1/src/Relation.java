c335de756be82861ca4bfc09ae5f6ab7a6ebc1e9
Iftu Rahman Bashar <tonkonblitz50@gmail.com>
2026-09-25 09:15:56 +0200

    Completato Relation - Passo 1

--- AlgebraRelazione1/src/Relation.java
+++ AlgebraRelazione1/src/Relation.java
@@ -48,19 +48,82 @@ public class Relation {
 
     }
 
-    // Da fare
 
-//    public Relation projection(ArrayList<String> keys) {
-//
-//    }
-//
-//    public Relation union(Relation other) {
-//
-//    }
-//
-//    public Relation difference(Relation other) {
-//
-//    }
+
+    public Relation projection(ArrayList<String> keys) {
+
+        Relation ris = new Relation();
+        ArrayList<Row> projectedRows = new ArrayList<>();
+
+        for (int i = 0; i < rows.size(); i++) {
+            ArrayList<String> newVals = new ArrayList<>();
+
+            for (int j = 0; j < keys.size(); j++) {
+
+                int index = header.indexOf(keys.get(j));
+                newVals.add(rows.get(i).getValue(index));
+            }
+
+            projectedRows.add(new Row(newVals));
+        }
+
+        ris.setHeader(keys);
+        ris.setRows(projectedRows);
+
+        return ris;
+    }
+
+
+    public Relation union(Relation other) {
+        Relation ris = new Relation();
+
+        if (!header.equals(other.getHeader())) {
+            return null;
+        }
+
+        ArrayList<Row> unionRows = new ArrayList<>();
+        for (int i = 0; i < rows.size(); i++) {
+            unionRows.add(rows.get(i));
+        }
+
+        for (int i = 0; i < other.getRows().size(); i++) {
+
+            Row r = other.getRows().get(i);
+
+            if (!unionRows.contains(r)) {
+                unionRows.add(r);
+            }
+        }
+
+        ris.setHeader(header);
+        ris.setRows(unionRows);
+
+        return ris;
+    }
+
+
+    public Relation difference(Relation other) {
+        Relation ris = new Relation();
+
+        if (!header.equals(other.getHeader())) {
+            return null;
+        }
+
+        ArrayList<Row> differenceRows = new ArrayList<>();
+
+        for (int i = 0; i < rows.size(); i++) {
+            Row r = rows.get(i);
+
+            if (!other.getRows().contains(r)) {
+                differenceRows.add(r);
+            }
+        }
+
+        ris.setHeader(header);
+        ris.setRows(differenceRows);
+
+        return ris;
+    }
 
     @Override
     public String toString() {
